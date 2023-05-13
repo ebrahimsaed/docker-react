d53d1191e10ab335a597589766b0a07879cc7a6d
Ebrahim Saed <Ebrahimsaed@outlook.com>
2023-05-13 10:22:35 +0300

    removed the failed test

--- src/App.test.js
+++ src/App.test.js
@@ -6,8 +6,3 @@ test("renders learn react link", () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
-test("renders my name text", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/my name text/i);
-  expect(linkElement).toBeInTheDocument();
-});
